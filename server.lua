66bce0ede4d466fd9f663c9f933536664b6aad76
MatkezZz <156377004+MatkezZzz@users.noreply.github.com>
2025-08-15 14:01:06 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,32 +3,33 @@ if Config.Framework == 'ESX' then
     ESX = exports.es_extended:getSharedObject()
 end
 
-lib.callback.register('matkez_speedcam:napisiKaznu', function(source, cena, razlog, stash, society)
-    if not source or cena >= 10000 then return end
+lib.callback.register('matkez_speedcam:napisiKaznu', function(source, kamera)
+    local cfg = Config.Kamere[kamera]
+    if not cfg then return false end
     if Config.SkiniOdmah then
         if Config.Framework == 'QBOX' then
-            exports.qbx_core:RemoveMoney(source, 'bank', cena, razlog)
+            exports.qbx_core:RemoveMoney(source, 'bank', cfg.cena, cfg.razlog)
         elseif Config.Framework == 'ESX' then 
             local xPlayer = ESX.GetPlayerFromId(source)
-            xPlayer.removeAccountMoney('bank', cena)
+            xPlayer.removeAccountMoney('bank', cfg.cena)
         end
-        if society then
+        if cfg.society then
             if Config.Framework == 'QBOX' then
-                exports['Renewed-Banking']:addAccountMoney(stash, cena)
+                exports['Renewed-Banking']:addAccountMoney(cfg.stash, cfg.cena)
             elseif Config.Framework == 'ESX' then 
-                TriggerEvent('esx_addonaccount:getSharedAccount', 'society_'..stash, function(acc)
-                    acc.addMoney(cena)
+                TriggerEvent('esx_addonaccount:getSharedAccount', 'society_'..cfg.stash, function(acc)
+                    acc.addMoney(cfg.cena)
                 end)
             end
         else
-            exports.ox_inventory:AddItem(stash, 'money', cena)
+            exports.ox_inventory:AddItem(cfg.stash, 'money', cfg.cena)
         end
     else
-        TriggerEvent("okokBilling:CreateCustomInvoice", source, cena, razlog, razlog, stash, stash)
+        TriggerEvent("okokBilling:CreateCustomInvoice", source, cfg.cena, cfg.razlog, cfg.razlog, cfg.stash, cfg.stash)
     end
     TriggerClientEvent('ox_lib:notify', source, {
         title = Config.Notify.notifTitle,
-        description = Config.Notify.notifDesc..cena..'$',
+        description = Config.Notify.notifDesc..cfg.cena..'$',
         duration = Config.Notify.duration,
         showDuration = true,
         position = Config.Notify.pos,
